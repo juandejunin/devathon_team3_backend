f6b4f0089e20b715aa98592ef6647c6057400c27
Jhoel Santiago Soncco Florez <jhoel.soncco.ext@expertiatravel.com>
2024-04-18 10:59:25 -0500

    architecture

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,23 @@
 import express from 'express';
-import http from 'http';
-import { Socket, Server as SocketIOServer } from 'socket.io';
-import { connectDB } from './db/db';
+import { DatabaseBootstrap } from './bootstrap/database.bootstrap';
+import { ServerBootstrap } from './bootstrap/server.bootstrap';
 
 const app = express();
-const server = http.createServer(app);
 
-const PORT = 3000;
+(async () => {
+  const serverBootstrap = new ServerBootstrap(app);
+  const databaseBootstrap = new DatabaseBootstrap();
 
-const io = new SocketIOServer(server);
+  try {
+    await Promise.all([
+      serverBootstrap.initialize(),
+      databaseBootstrap.initialize()
+    ]);
 
-app.get('/', (req: any, res: any) => {
-  console.log('Se ha recibido una solicitud GET en la ruta /');
-  res.send('¡Hola desde Express!');
-});
-
-app.use((err: any, req: any, res: any, next: any) => {
-  console.error(err.stack);
-  res.status(500).send('Algo salió mal!');
-});
-
-server.listen(PORT, () => {
-  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
-});
-
-connectDB();
-io.on('connection', (socket: Socket) => {
-  console.log('Un cliente se ha conectado');
-
-  socket.on('mensaje', (data: any) => {
-    console.log('Mensaje recibido:', data);
-  });
-
-  socket.emit('bienvenida', '¡Bienvenido a la app de preguntas y respuestas!');
-});
-
-process.on('unhandledRejection', (err) => {
-  console.error(err);
-  server.close(() => process.exit(1));
-});
+    console.log('Server and database are running');
+  } catch (err) {
+    console.error('Error occurred during initialization:', err);
+    await databaseBootstrap.close();
+    process.exit(1);
+  }
+})();
