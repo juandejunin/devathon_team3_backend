c4a5b82f1837a200fbc6065b23981951864f1bf4
Ramiro Alejandro Alvarez <ramiro.al.alvarez@gmail.com>
2024-04-18 22:24:29 -0300

    test: / returns status code 200

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,6 +5,7 @@ const index   = require('../src/index');
 describe ('GET / correctly', () => {
     test('Get / with 200 status', async () => {
         const response = await request(index).get('/').send();
+        // '/' returns status code 200 (success);
         expect(response.statusCode).toBe(200);
     })
 
