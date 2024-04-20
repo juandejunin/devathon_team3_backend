441980f5c4e3c51269d4c3665f12bfc13a698e68
Ramiro Alejandro Alvarez <ramiro.al.alvarez@gmail.com>
2024-04-20 02:02:31 -0300

    adding OOP to db file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import app from './app';
 import { DatabaseBootstrap } from './bootstrap/database.bootstrap';
 import { ServerBootstrap } from './bootstrap/server.bootstrap';
@@ -26,54 +25,3 @@ import { SocketBootstrap } from './bootstrap/socket.bootstrap';
     process.exit(1);
   }
 })();
-=======
-import express from 'express';
-import http from 'http';
-import { Socket, Server as SocketIOServer } from 'socket.io';
-import  mongoDB  from './db/db';
-
-const app = express();
-const server = http.createServer(app);
-const PORT = 8000;
-const io = new SocketIOServer(server);
-
-
-
-
-app.get('/', (req: Request, res: Response | any) => {
-  console.log('Se ha recibido una solicitud GET en la ruta /');
-  res.send('¡Hola desde Express!');
-});
-
-app.use((err: Error | any, req: any, res: any, next: any) => {
-  console.error(err.stack);
-  res.status(500).send('Algo salió mal!');
-});
-
-
-mongoDB.connectDB() ;
-
-
-server.listen(PORT, () => {
-  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
-});
-
-io.on('connection', (socket: Socket) => {
-  console.log('Un cliente se ha conectado');
-
-  socket.on('mensaje', (data: any) => {
-    console.log('Mensaje recibido:', data);
-  });
-  socket.emit('bienvenida', '¡Bienvenido a la app de preguntas y respuestas!');
-});
-
-process.on('unhandledRejection', (err: unknown) => {
-  console.error(err);
-  server.close(() => process.exit(1));
-});
-
-module.exports = app.get('/', (req: Request, res: Response | any) => {
-  console.log('Se ha recibido una solicitud GET en la ruta /');
-  res.send('¡Hola desde Express!');
-});
->>>>>>> e94e555 (agregando POO a db)
