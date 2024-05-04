1116f05897b995999af476934a4f06f2412e4c9f
juandejunin <juandejunin75@gmail.com>
2024-05-04 08:19:46 +0200

    feat: added database connection checking

--- __test__/create-room.test.ts
+++ __test__/create-room.test.ts
@@ -1,29 +1,36 @@
-import request from 'supertest'; // Este módulo nos permite hacer solicitudes HTTP para probar nuestras rutas
-import { RoomRoutes } from '../src/presentation/room.routes';
-
-const roomRoutes = new RoomRoutes();
-
+import request from 'supertest'; 
+import mongoose from 'mongoose';
+import app from '../src/app';
 
 describe('POST /create return 201 ', () => {
+  beforeAll(async () => {
+    await mongoose.connect(
+      'mongodb://${username}:${password}@${host}:${port}/'
+    );
+  });
 
-  test('POST /create devuelve un status code 201', async () => {  
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  test('should connect to the database', async () => {
+    const isConnected = mongoose.connection.readyState === 1;
+
+    expect(isConnected).toBe(true);
+  });
+
+  test('POST /create devuelve un status code 201', async () => {
     const roomQuery =
     {
       roomName : 'RoomName',
       userName : 'userName'
     }
-    
-    const response = 
-    await request(roomRoutes.router)
-    .post('/create')
-    .send(roomQuery)
-    .set('Content-Type', 'application/json')
-    .set('Accept', 'application/json')
+    const response = await request(app)
+      .post('/api/v1/room/create')
+      .send(roomQuery)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
     expect(response.status).toBe(201);
-
   });
-
 });
-
-
-
